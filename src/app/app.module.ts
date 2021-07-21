e3575e12cd40ed2d52d9dd14545f70b3f9a5c277
Benjamin Heich <benjaminheich@gmail.com>
2021-07-21 10:08:29 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PageListComponent } from './page-list/page-list.component';
+import { TemplateTodoComponent } from './template/template-todo/template-todo.component';
+import { TemplateHeaderComponent } from './template/template-header/template-header.component';
+import { TemplateTodoFormComponent } from './template/template-todo-form/template-todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageListComponent,
+    TemplateTodoComponent,
+    TemplateHeaderComponent,
+    TemplateTodoFormComponent,
   ],
   imports: [
     BrowserModule
